fc79c61dbba747e2d1e06137de07ba8101c911e6
Ayman Hussein <ayman.kanaaneh@gmail.com>
2026-02-14 17:36:13 +0200

    feat: solve question

---
+++ Python/subarraySum-560.py
@@ -0,0 +1,34 @@
+#https://leetcode.com/problems/subarray-sum-equals-k/
+#Level: Medium
+#Time Complexity: O(n), n = len(nums)
+#Space Complexity: O(n), n = len(nums)
+
+
+
+class Solution:
+
+    def subarraySum(self, nums: List[int], k: int) -> int:
+
+        currSum = 0
+        prefixSum = {}
+        prefixSum[currSum] = 1
+        numsLen = len(nums)
+        count = 0
+        diff = 0
+
+        for i in range(numsLen):
+            currSum+=nums[i]
+            diff = currSum-k
+
+            count+=prefixSum.get(diff, 0)
+            
+            prefixSum[currSum] = prefixSum.get(currSum, 0) + 1
+
+            
+        return count
+
+
+
+
+
+        
